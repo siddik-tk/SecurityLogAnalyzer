c14d409879f03b56852ba5fca9708db800592c9b
siddik-tk <siddik3tk@gmail.com>
2026-06-03 00:50:40 +0530

    update

--- src/LogAnalyzer.cpp
+++ src/LogAnalyzer.cpp
@@ -1,6 +1,8 @@
 #include "LogAnalyzer.h"
 #include <fstream>
+#include <sstream>
 #include <iostream>
+#include <regex>
 using namespace std;
 
 void LogAnalyzer::readlogfile(const string& filename){
@@ -8,8 +10,12 @@ void LogAnalyzer::readlogfile(const string& filename){
     ifstream logfile(filename);
     if (logfile.is_open()){
         string line;
+        regex pattern(R"((Failed|Success).*?(\d+.\d+.\d+.\d+))");
         while(getline(logfile,line)){
-            cout << line << endl;
+            smatch match;
+            if(regex_search(line, match, pattern)){
+                cout << "[INFO] " << match[1] << " login attempt from IP: " << match[2] << endl;
+            }
         }
     }
     else{
